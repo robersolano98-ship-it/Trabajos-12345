b3f23c251dff76dae8664c551a0ff2613bd4204f
robersolano98-ship-it <robersolano98@gmail.com>
2026-09-24 10:01:26 -0700

    Recorrido de matricez en 2d
    JAVA

---
+++ 10_recorrido_matriz_2d.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int[][] mat = {{1,2,3},{4,5,6},{7,8,9}};
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++) System.out.print(mat[i][j]+" ");
+            System.out.println();
+        }
+    }
+}
