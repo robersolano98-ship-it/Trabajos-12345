d2206525f05cc50658bd16c81ad1d13582e9a781
robersolano98-ship-it <robersolano98@gmail.com>
2026-09-23 22:26:50 -0700

    recorrido secuencial
    C#

---
+++ 1_recorrido_secuencial.cs
@@ -0,0 +1,7 @@
+using System;
+class Program {
+    static void Main(){
+        int[] arr = {10,20,30,40,50};
+        for(int i=0; i<arr.Length; i++) Console.WriteLine($"arr[{i}] = {arr[i]}");
+    }
+}
