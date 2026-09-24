94b6234af3148b1bdf6fb522dd7b01f523f53bd5
robersolano98-ship-it <robersolano98@gmail.com>
2026-09-24 09:38:24 -0700

    ordenamiento bubble
    JAVA SCRIPT

---
+++ 14_ordenamiento_bubble.js
@@ -0,0 +1,7 @@
+function bubble(arr){
+  for(let i=0;i<arr.length;i++)
+    for(let j=0;j<arr.length-i-1;j++)
+      if(arr[j]>arr[j+1]) [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
+  return arr;
+}
+console.log(bubble([64,34,25,12,22,11,90]));
